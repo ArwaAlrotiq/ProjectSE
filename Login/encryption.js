d6d1547c4e5f14aaf15955b21f9b9717dbb04b66
nouraWael0 <nouraa2000666@gmail.com>
2026-03-09 02:44:09 +0300

    Fix encryption.js - Noura

---
+++ Login/encryption.js
@@ -0,0 +1,9 @@
+function simpleHash(str) {
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+        const char = str.charCodeAt(i);
+        hash = ((hash << 5) - hash) + char;
+        hash = hash & hash;
+    }
+    return hash.toString(16);
+}
